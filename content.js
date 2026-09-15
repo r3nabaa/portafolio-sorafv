2d9d37e2480c43f4f9e5a8afa03b6333a91bed36
r3nabaa <renatoxdeee75@gmail.com>
2026-09-14 21:16:37 -0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,20 @@
+/* EDITA ESTE ARCHIVO PARA PERSONALIZAR Y AÑADIR CONTENIDO. */
+window.portfolioData = {
+  skinHeadImage: "https://mc-heads.net/avatar/sorafv/100", // URL de la cabeza de tu skin.
+  projects: [
+    {
+      number: "01",
+      category: "ESTUDIO ORIGINAL",
+      title: "SORA|STUDIO",
+      description: "Fundado por mí y “Moncho”. Es mi proyecto original, con el futuro evento Squid Sora Games.",
+      image: "soraicon.png",
+    },
+    {
+      number: "02",
+      category: "COMUNIDAD / MODS",
+      title: "RIVER|COMMUNITY",
+      description: "Comunidad de mods de Minecraft para un evento cuya información por ahora es confidencial. Soy owner y su creador es Zachbtw.",
+      image: "rivericon.png",
+    },
+  ],
+};
